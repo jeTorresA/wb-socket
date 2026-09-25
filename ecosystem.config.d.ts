acac91897975747863747078f93d27c6cac57c2f
Juan Esteban Torres <juane.torresa@gmail.com>
2026-09-25 12:21:54 -0500

    deploy: dist dfd7adf

--- ecosystem.config.d.ts
+++ ecosystem.config.d.ts
@@ -1,1 +1,1 @@
-declare const nodeInterpreter: string;
+declare const nodeInterpreter = "/root/.nvm/versions/node/v16.20.2/bin/node && pm2 start ecosystem.config.js --env production";
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-const nodeInterpreter = process.env.PM2_NODE_INTERPRETER;
+const nodeInterpreter = '/root/.nvm/versions/node/v16.20.2/bin/node && pm2 start ecosystem.config.js --env production';
 module.exports = {
     apps: [
         {
