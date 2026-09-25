3b9125b7d64b3c7a6250e53cd3c775a999358577
Juan Esteban Torres <juane.torresa@gmail.com>
2026-09-25 12:24:44 -0500

    deploy: dist e206f70

--- ecosystem.config.d.ts
+++ ecosystem.config.d.ts
@@ -1,1 +1,1 @@
-declare const nodeInterpreter = "/root/.nvm/versions/node/v16.20.2/bin/node && pm2 start ecosystem.config.js --env production";
+declare const nodeInterpreter = "/root/.nvm/versions/node/v16.20.2/bin/node";
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-const nodeInterpreter = '/root/.nvm/versions/node/v16.20.2/bin/node && pm2 start ecosystem.config.js --env production';
+const nodeInterpreter = '/root/.nvm/versions/node/v16.20.2/bin/node';
 module.exports = {
     apps: [
         {
